e005ba157ed0b96d45aa4c923260ac08ec93e2ce
Kaen Pawanachai <pawaox4th@gmail.com>
2023-11-03 17:38:21 +0700

    🤡 (Test): Test

--- src/index.ts
+++ src/index.ts
@@ -3,6 +3,7 @@ import { Elysia } from 'elysia'
 import { readFileSync } from 'fs'
 import { Resolvers } from './__generated__/resolvers-types'
 import axios from 'axios'
+import serverTiming from '@elysiajs/server-timing'
 
 const typeDefsSchema = readFileSync(
     import.meta.dir.concat('/features/book/schema.graphql'),
@@ -13,36 +14,52 @@ const typeDefsSchema = readFileSync(
 
 const resolvers: Resolvers = {
     Query: {
-        books: async (_, args) => {
-            // {
-            //   "userId": 1,
-            //   "id": 1,
-            //   "title": "delectus aut autem",
-            //   "completed": false
-            //   }
-            const { title } = await axios
-                .get('https://jsonplaceholder.typicode.com/todos/1')
-                .then((res) => res.data)
+        // books: async (_, args) => {
+        //     const { title } = await axios
+        //         .get('https://jsonplaceholder.typicode.com/todos/1')
+        //         .then((res) => res.data)
 
-            return [
-                {
-                    title,
-                    author: 'Kaen',
-                    age: 20,
-                },
-            ]
-        },
+        //     return [
+        //         {
+        //             title,
+        //             author: 'Kaen',
+        //             age: 20,
+        //         },
+        //     ]
+        // },
         users: async (_, { id }) => {
+            const start = performance.now()
             const response = await axios
                 .get(`https://jsonplaceholder.typicode.com/todos/${id}`)
                 .then((res) => res.data)
 
-            console.log(response)
+            // const response = await fetch(
+            //     `https://jsonplaceholder.typicode.com/todos/${id}`
+            // ).then((res) => res.json())
+
+            // console.log(response)
+            console.log(`[LOG] 🟢 ${performance.now() - start} ms.`)
             return [response]
         },
     },
 }
 const app = new Elysia()
+    .trace(async ({ beforeHandle, request, response, set }) => {
+        const { children, time: start, end } = await beforeHandle
+        for (const child of children) {
+            const { time: start, end, name } = await child
+
+            console.log(name, 'took', (await end) - start, 'ms')
+        }
+
+        console.log('beforeHandle took', (await end) - start)
+
+        const re = await request
+        console.log('[LOG] 🚀  request :', await re)
+
+        set.headers
+    })
+
     .use(
         apollo({
             typeDefs: typeDefsSchema,
